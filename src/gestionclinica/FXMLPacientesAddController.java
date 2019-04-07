a306cc4e7ec55986d7645c6404c337f4b625ce78
aucejito <38977418+aucejito@users.noreply.github.com>
2019-04-07 23:19:05 +0200

    Add files via upload

---
+++ src/gestionclinica/FXMLPacientesAddController.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestionclinica;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+
+/**
+ * FXML Controller class
+ *
+ * @author Carlos
+ */
+public class FXMLPacientesAddController implements Initializable {
+
+    @FXML
+    private ChoiceBox<?> diasSemana;
+    
+    
+    
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        //"Lunes", "Martes","Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"));
+    }    
+    
+    
+}
